d7c6e789d9d3b26a3305d474b1ebd7d299c4033b
Soham Walam <147302133+SohamWalam11@users.noreply.github.com>
2025-07-30 19:47:34 +0530

    certs storage

---
+++ src/lib/certs.ts
@@ -0,0 +1,70 @@
+export const certifications = [
+  {
+    title: "AWS Cloud Services",
+    image: "/certs/AWS_cloud.png",
+  },
+  {
+    title: "PyTorch (OpenCV)",
+    image: "/certs/PyTorch.png",
+  },
+  {
+    title: "SQL Intermediate Test",
+    image: "/certs/SQL.png",
+  },
+  {
+    title: "ArcGIS Python | Spatial Data Science",
+    image: "/certs/ArcGIS.png",
+  },
+  {
+    title: "Machine Learning Bootcamp (S4DS)",
+    image: "/certs/Soham Kishor Walam.png",
+  },
+  {
+    title: "Data Science & Analytics (HP)",
+    image: "/certs/HP_Data.jpg",
+  },
+  {
+    title: "AWS Solution Architecture",
+    image: "/certs/aws-architecture.png",
+  },
+  {
+     title: "Tensorflow (OpenCV)",
+    image: "/certs/TensorFlow & Keras.png",
+  },
+  {
+    title: "Develop ML Solutions (AWS)",
+    image: "/certs/Develop ML Solutions.png",
+  },
+  {
+    title : "Optimize ML Models (AWS)",
+    image : "/certs/Optimize ML Models.png",
+  },
+  {
+    title : "Build with AI (Sayler)",
+    image : "/certs/Build with AI.png",
+  },
+  {
+    title: "GenAI with Diffusion Models (AWS)",
+    image: "/certs/GenAI with Diffusion models.png",
+  },
+  {
+    title: "Python Basic",
+    image: "/certs/Test.png",
+  },
+  {
+     title: "Data Engineering (AWS)",
+    image: "/certs/Data Engg.png",
+  },
+  {
+    title : "Data Science (SkillUp)",
+    image : "/certs/Data Science SkillUp.png",
+  },
+  {
+    title : "Python and AI (DevTown)",
+    image : "/certs/Python and AI.png",
+  },
+  {
+    title : "GenAI with LLMs (PreGrad)",
+    image : "/certs/GenAI.png",
+  }
+];
